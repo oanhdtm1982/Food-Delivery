4f9c9137d8acd28050c29834b38fdf86a8d75775
MinhhOanhh <19521982@gm.uit.edu.vn>
2022-08-23 10:51:45 +0700

    add.

--- lib/pages/restaurant_detail/components/detail_restaurant.dart
+++ lib/pages/restaurant_detail/components/detail_restaurant.dart
@@ -28,144 +28,132 @@ class DetailRestaurant extends StatelessWidget {
     return Expanded(
       child: Align(
         alignment: Alignment.bottomCenter,
-        child: SingleChildScrollView(
-          child: Expanded(
-            child: Container(
-              height: SizeConfig.screenHeight! * 0.7,
-              width: SizeConfig.screenWidth! * 1,
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.only(
-                    topRight: Radius.circular(40),
-                    topLeft: Radius.circular(40)),
-                color: Colors.white,
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          children: const [
-                            ButtonText(
-                              textFilter: 'Popular',
-                            ),
-                          ],
-                        ),
-                        const Text(
-                          'Wijie Bar and Resto',
-                          style: textNameProfile,
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        const Text(
-                          'Most whole Alaskan Red King Crabs get broken down into legs, claws, and lump meat. We offer all of these options as well in our online shop, but there is nothing like getting the whole . . . .',
-                          style: textDescriptionRestaurant,
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                      ],
+        child: Expanded(
+          child: Container(
+            height: SizeConfig.screenHeight! * 0.7,
+            width: SizeConfig.screenWidth! * 1,
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(40), topLeft: Radius.circular(40)),
+              color: Colors.white,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: const [
+                          ButtonText(
+                            textFilter: 'Popular',
+                          ),
+                        ],
+                      ),
+                      const Text(
+                        'Wijie Bar and Resto',
+                        style: textNameProfile,
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      const Text(
+                        'Most whole Alaskan Red King Crabs get broken down into legs, claws, and lump meat. We offer all of these options as well in our online shop, but there is nothing like getting the whole . . . .',
+                        style: textDescriptionRestaurant,
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                    ],
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const ExploreFoodScreen()));
+                  },
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(SizeConfig.screenWidth! * 0.05,
+                        0, SizeConfig.screenWidth! * 0.05, 0),
+                    child: const TitleGroup(
+                      mainTitle: 'Popular Menu',
                     ),
                   ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const ExploreFoodScreen()));
-                    },
-                    child: Padding(
-                      padding: EdgeInsets.fromLTRB(
-                          SizeConfig.screenWidth! * 0.05,
-                          0,
-                          SizeConfig.screenWidth! * 0.05,
-                          0),
-                      child: const TitleGroup(
-                        mainTitle: 'Popular Menu',
+                ),
+                SizedBox(
+                  height: SizeConfig.screenWidth! * 0.4,
+                  width: SizeConfig.screenWidth! * 1,
+                  child: Expanded(
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 2,
+                      itemBuilder: (context, index) => GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamedAndRemoveUntil(
+                              context,
+                              RestaurantDetailScreen.routeName,
+                              (route) => false);
+                        },
+                        child: Padding(
+                          padding: EdgeInsets.fromLTRB(
+                              SizeConfig.screenWidth! * 0.05,
+                              15,
+                              SizeConfig.screenWidth! * 0.05,
+                              20),
+                          child: RestaurantCard(
+                              restaurantModel:
+                                  restaurantNotifier.restaurantList[index],
+                              onPress: () {}),
+                        ),
                       ),
                     ),
                   ),
-                  SizedBox(
-                    height: SizeConfig.screenWidth! * 0.4,
-                    width: SizeConfig.screenWidth! * 1,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const ExploreFoodScreen()));
+                  },
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(SizeConfig.screenWidth! * 0.05,
+                        0, SizeConfig.screenWidth! * 0.05, 0),
+                    child: const TitleGroup(
+                      mainTitle: 'Testimonials',
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: SizeConfig.screenWidth! * 1,
+                  width: SizeConfig.screenWidth! * 1,
+                  child: MediaQuery.removePadding(
+                    removeTop: true,
+                    removeBottom: true,
+                    context: context,
                     child: Expanded(
                       child: ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        itemCount: 2,
-                        itemBuilder: (context, index) => GestureDetector(
-                          onTap: () {
-                            Navigator.pushNamedAndRemoveUntil(
-                                context,
-                                RestaurantDetailScreen.routeName,
-                                (route) => false);
-                          },
-                          child: Padding(
-                            padding: EdgeInsets.fromLTRB(
-                                SizeConfig.screenWidth! * 0.05,
-                                15,
-                                SizeConfig.screenWidth! * 0.05,
-                                20),
-                            child: RestaurantCard(
-                                restaurantModel:
-                                    restaurantNotifier.restaurantList[index],
-                                onPress: () {}),
-                          ),
-                        ),
+                        scrollDirection: Axis.vertical,
+                        itemCount: chatDemo.length,
+                        itemBuilder: (context, index) => TestimonialCard(
+                            chatModel: chatDemo[index],
+                            onPress: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          const ChatDetailScreen()));
+                            }),
                       ),
                     ),
                   ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const ExploreFoodScreen()));
-                    },
-                    child: Padding(
-                      padding: EdgeInsets.fromLTRB(
-                          SizeConfig.screenWidth! * 0.05,
-                          0,
-                          SizeConfig.screenWidth! * 0.05,
-                          0),
-                      child: const TitleGroup(
-                        mainTitle: 'Testimonials',
-                      ),
-                    ),
-                  ),
-                  // TestimonialsCard(
-                  //   testimonialModel: testimonialDemo[0],
-                  // ),
-                  SizedBox(
-                    height: SizeConfig.screenWidth! * 1,
-                    width: SizeConfig.screenWidth! * 1,
-                    child: MediaQuery.removePadding(
-                      removeTop: true,
-                      removeBottom: true,
-                      context: context,
-                      child: Expanded(
-                        child: ListView.builder(
-                          scrollDirection: Axis.vertical,
-                          itemCount: chatDemo.length,
-                          itemBuilder: (context, index) => ChatCard(
-                              chatModel: chatDemo[index],
-                              onPress: () {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) =>
-                                            const ChatDetailScreen()));
-                              }),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
--- lib/pages/restaurant_detail/components/testimonials_card.dart
+++ lib/pages/restaurant_detail/components/testimonials_card.dart
@@ -1,66 +1,71 @@
 import 'package:flutter/material.dart';
-import 'package:food_delivery/models/testimonial_model.dart';
+import 'package:food_delivery/constants/colors/colors.dart';
+import 'package:food_delivery/constants/styles/text_styles.dart';
+import 'package:food_delivery/models/chat_model.dart';
+import 'package:food_delivery/widgets/size_config.dart';
 
-import '../../../constants/colors/colors.dart';
-import '../../../constants/styles/text_styles.dart';
-import '../../../widgets/size_config.dart';
-
-class TestimonialsCard extends StatelessWidget {
-  const TestimonialsCard({Key? key, required this.testimonialModel})
-      : super(key: key);
-  final TestimonialModel testimonialModel;
-
+class TestimonialCard extends StatelessWidget {
+  const TestimonialCard({
+    Key? key,
+    required this.onPress,
+    required this.chatModel,
+  }) : super(key: key);
+  final ChatModel chatModel;
+  final Function() onPress;
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
-      child: Container(
-          height: SizeConfig.screenWidth! * 0.2,
-          width: SizeConfig.screenWidth! * 0.4,
-          decoration: BoxDecoration(
-              borderRadius: const BorderRadius.all(Radius.circular(15)),
-              color: appBackgroundButtonColor.withOpacity(0.1),
-              boxShadow: const [
-                BoxShadow(color: Colors.white, blurRadius: 15)
-              ]),
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(15, 10, 15, 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Image.asset('assets/images/food_1.png'),
-                const SizedBox(
-                  width: 15,
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      'chatModel.chatName',
-                      style: titleFood,
-                    ),
-                    const SizedBox(
-                      height: 8,
-                    ),
-                    Text('chatModel.messageLast', style: descRestaurantName),
-                  ],
-                ),
-                Expanded(
-                    child: Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
-                  child: Align(
-                      alignment: Alignment.topRight,
-                      child: Text(
-                        'chatModel.time',
-                        textAlign: TextAlign.right,
-                        style: descRestaurantName,
-                      )),
-                )),
-              ],
-            ),
-          )),
+    return InkWell(
+      onTap: onPress,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
+        child: Container(
+            height: SizeConfig.screenWidth! * 0.2,
+            width: SizeConfig.screenWidth! * 0.4,
+            decoration: BoxDecoration(
+                borderRadius: const BorderRadius.all(Radius.circular(15)),
+                color: appBackgroundButtonColor.withOpacity(0.1),
+                boxShadow: const [
+                  BoxShadow(color: Colors.white, blurRadius: 15)
+                ]),
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(15, 10, 15, 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset(chatModel.chatUrlImage),
+                  const SizedBox(
+                    width: 15,
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        chatModel.chatName,
+                        style: titleFood,
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      Text(chatModel.messageLast, style: descRestaurantName),
+                    ],
+                  ),
+                  Expanded(
+                      child: Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                    child: Align(
+                        alignment: Alignment.topRight,
+                        child: Text(
+                          chatModel.time,
+                          textAlign: TextAlign.right,
+                          style: descRestaurantName,
+                        )),
+                  )),
+                ],
+              ),
+            )),
+      ),
     );
   }
 }
